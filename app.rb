c896a9cb32b6511021ce7c911605430a217d473b
Naquaiviera Willis <naquaiviera.willis@gmail.com>
2018-07-17 16:50:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+  "my name is Quai."
+end
+get '/hometown' do 
+  "My hometown is Milwaukee, Wisconsin."
+end
+get '/favorite-song' do
+  "My favorite song is Wet Dreams by J Cole."
+ end
 end
